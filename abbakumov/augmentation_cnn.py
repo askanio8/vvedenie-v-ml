3b4132a815581d4eb3c69f5ea8d8e303e0962735
askanio8@gmail.com <askanio8@gmail.com>
2021-05-18 01:38:14 +0300

    л3

--- abbakumov/augmentation_cnn.py
+++ abbakumov/augmentation_cnn.py
@@ -4,35 +4,60 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras import models
 from tensorflow.keras import layers
 from tensorflow.keras.callbacks import TensorBoard
+import random
 
 # Перемещаем картинки в папки своих классов
-# cats = [shutil.move("./dogscats/train/train" + "/" + f, "./dogscats/train/cat" + "/" + f)
-#        for f in os.listdir("./dogscats/train/train") if f.find("cat") != -1]
-# dogs = [shutil.move("./dogscats/train/train" + "/" + f, "./dogscats/train/dog" + "/" + f)
-#        for f in os.listdir("./dogscats/train/train") if f.find("dog") != -1]
+#cats = [shutil.move("./dogscats/train/train" + "/" + f, "./dogscats/train/cat" + "/" + f)
+#       for f in os.listdir("./dogscats/train/train") if f.find("cat") != -1]
+#dogs = [shutil.move("./dogscats/train/train" + "/" + f, "./dogscats/train/dog" + "/" + f)
+#       for f in os.listdir("./dogscats/train/train") if f.find("dog") != -1]
 
-# Из паок делаем выборки обучаюзую и валидации
-datagen = ImageDataGenerator(rescale=1. / 255, validation_split=0.1)
-train_generator = datagen.flow_from_directory("./dogscats/train", classes=['dog', 'cat'],
+# Выделяем из обучающей выборки выборку валидации
+#cats = [f for f in os.listdir("./dogscats/train/cat")]
+#dogs = [f for f in os.listdir("./dogscats/train/dog")]
+
+#i = 0
+#while i < 1500:
+#    ind = random.randint(0, 12499)
+#    if os.path.isfile("./dogscats/train/cat" + "/" + cats[ind]):
+#        shutil.move("./dogscats/train/cat" + "/" + cats[ind], "./dogscats/validation/cat" + "/" + cats[ind])
+#        i = i + 1
+
+#i = 0
+#while i < 1500:
+#    ind = random.randint(0, 12499)
+#    if os.path.isfile("./dogscats/train/dog" + "/" + dogs[ind]):
+#        shutil.move("./dogscats/train/dog" + "/" + dogs[ind], "./dogscats/validation/dog" + "/" + dogs[ind])
+#        i = i + 1
+
+
+# Из папок делаем выборки обучающую и валидации. Параметры кроме первого - случайные преобразования.
+# Для метода fit каждую эпоху будет генерироваться выборка со случайными преобразованиями в заданных пределах
+train_datagen = ImageDataGenerator(rescale=1. / 255)
+
+
+val_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow_from_directory("./dogscats/train", classes=['dog1000', 'cat1000'],
                                                     target_size=(150, 150), batch_size=20,
-                                                    class_mode='binary', subset='training')
-val_generator = datagen.flow_from_directory("./dogscats/train", classes=['dog', 'cat'],
-                                                    target_size=(150, 150), batch_size=20,
-                                                    class_mode='binary', subset='validation')
+                                                    class_mode='binary', color_mode='grayscale')
+val_generator = val_datagen.flow_from_directory("./dogscats/validation", classes=['dog1000', 'cat1000'],
+                                                    target_size=(150, 150),
+                                                    class_mode='binary', color_mode='grayscale')
 
 # Модель
 model = models.Sequential([
     layers.Conv2D(32, (3,3), activation='relu',
-                  input_shape=(150,150,3)),
-    layers.MaxPooling2D((2,2)),
+                  input_shape=(150,150,1)),
+    layers.MaxPooling2D((3, 3)),
     layers.Conv2D(64, (3,3), activation='relu'),
-    layers.MaxPooling2D((2,2)),
+    layers.MaxPooling2D((3,3)),
     layers.Conv2D(128, (3,3), activation='relu'),
-    layers.MaxPooling2D((2,2)),
+    layers.MaxPooling2D((3, 3)),
     layers.Conv2D(128, (3,3), activation='relu'),
-    layers.MaxPooling2D((2,2)),
     layers.Flatten(),
-    layers.Dense(512, 'relu'),
+    layers.Dense(1024, 'relu'),
+    layers.Dense(1024, 'relu'),
     layers.Dense(1, 'sigmoid') # 1 нейрон – кошка или собака
 ])
 
@@ -45,7 +70,9 @@ tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
 
 model.fit(
     train_generator,
-    steps_per_epoch=100,
+    steps_per_epoch=50,
     epochs=30,
     validation_data=val_generator,
     callbacks=[tensorboard_callback])
+
+# Дообучение на отфильтрованных данных
